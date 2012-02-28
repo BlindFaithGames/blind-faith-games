088a1851cbdc53c4cec73af9b4777d4e482360a7
Gloria <gepozuelo@gmail.com>
2012-02-28 13:30:22 +0100

    + Formularios done!

--- Minesweeper/Minesweeper-Android/src/com/minesweeper/FormActivity.java
+++ Minesweeper/Minesweeper-Android/src/com/minesweeper/FormActivity.java
@@ -17,31 +17,31 @@ import android.widget.Button;
 import android.widget.MultiAutoCompleteTextView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.Toast;
 
 public class FormActivity extends Activity implements OnClickListener,
 		OnFocusChangeListener {
 
 	private TTS textToSpeech;
 
-	private Button buttonNext1, buttonNext2, buttonFinish;
-	private MultiAutoCompleteTextView ans14;
+	private Button buttonNext1, buttonFinish;
+	private MultiAutoCompleteTextView ans11;
 
 	private RadioGroup ans1, ans2, ans3, ans4, ans5, ans6, ans7, ans8, ans9,
-			ans10, ans11, ans12, ans13;
+			ans10;
 	private RadioButton r1Selected, r2Selected, r3Selected, r4Selected,
 			r5Selected, r6Selected, r7Selected, r8Selected, r9Selected,
-			r10Selected, r11Selected, r12Selected, r13Selected;
+			r10Selected;
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		
-		// If the user wants to send another form, firstly we remove the old information
+
+		// If the user wants to send another form, firstly we remove the old
+		// information
 		Log.getLog().clearAnswers();
-		
+
 		setScreenContent(R.layout.form1);
 
 		buttonNext1 = (Button) findViewById(R.id.buttonNext1);
@@ -52,9 +52,10 @@ public class FormActivity extends Activity implements OnClickListener,
 		textToSpeech = (TTS) getIntent().getParcelableExtra(
 				MinesweeperActivity.KEY_TTS);
 		textToSpeech.setContext(this);
-		textToSpeech.setInitialSpeech(getString(R.string.form_label));
-		
-		AnalyticsManager.getAnalyticsManager(this).registerPage(MinesweeperAnalytics.FORM_ACTIVITY);
+		textToSpeech.setInitialSpeech(getString(R.string.form_label) + "," + getString(R.string.form_intro));
+
+		AnalyticsManager.getAnalyticsManager(this).registerPage(
+				MinesweeperAnalytics.FORM_ACTIVITY);
 	}
 
 	@Override
@@ -67,19 +68,21 @@ public class FormActivity extends Activity implements OnClickListener,
 			setScreenContent(R.layout.form2);
 			InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 			// only will trigger it if no physical keyboard is open
-			mgr.showSoftInput(ans14, InputMethodManager.SHOW_IMPLICIT);
+			mgr.showSoftInput(ans11, InputMethodManager.SHOW_IMPLICIT);
 			buttonFinish = (Button) findViewById(R.id.button_finish);
 			buttonFinish.setOnClickListener(this);
 			buttonFinish.setOnFocusChangeListener(this);
 			done = true;
-			AnalyticsManager.getAnalyticsManager().registerAction(MinesweeperAnalytics.FORM_EVENTS, MinesweeperAnalytics.CLICK, 
-					"Next Button", 0);
+			AnalyticsManager.getAnalyticsManager().registerAction(
+					MinesweeperAnalytics.FORM_EVENTS,
+					MinesweeperAnalytics.CLICK, "Next Button", 0);
 			break;
 		case (R.id.button_finish):
 			saveResults(R.layout.form2);
 			done = true;
-			AnalyticsManager.getAnalyticsManager().registerAction(MinesweeperAnalytics.FORM_EVENTS, MinesweeperAnalytics.CLICK, 
-					"Finish button", 0);
+			AnalyticsManager.getAnalyticsManager().registerAction(
+					MinesweeperAnalytics.FORM_EVENTS,
+					MinesweeperAnalytics.CLICK, "Finish button", 0);
 			break;
 		}
 		if (!done) {
@@ -89,20 +92,22 @@ public class FormActivity extends Activity implements OnClickListener,
 				textToSpeech.speak(getString(R.string.q1_context));
 			else if (groupContainId(ans2, v.getId()))
 				textToSpeech.speak(getString(R.string.q2_context));
+			else if (ans3 != null && groupContainId(ans3, v.getId()))
+				textToSpeech.speak(getString(R.string.q3_context));
 			else if (ans4 != null && groupContainId(ans4, v.getId()))
 				textToSpeech.speak(getString(R.string.q4_context));
 			else if (ans5 != null && groupContainId(ans5, v.getId()))
 				textToSpeech.speak(getString(R.string.q5_context));
 			else if (ans6 != null && groupContainId(ans6, v.getId()))
 				textToSpeech.speak(getString(R.string.q6_context));
+			else if (ans7 != null && groupContainId(ans7, v.getId()))
+				textToSpeech.speak(getString(R.string.q7_context));
 			else if (ans8 != null && groupContainId(ans8, v.getId()))
 				textToSpeech.speak(getString(R.string.q8_context));
 			else if (ans9 != null && groupContainId(ans9, v.getId()))
 				textToSpeech.speak(getString(R.string.instructions_label));
 			else if (ans10 != null && groupContainId(ans10, v.getId()))
 				textToSpeech.speak(getString(R.string.q10_context));
-			else if (ans12 != null && groupContainId(ans12, v.getId()))
-				textToSpeech.speak(getString(R.string.q12_context));
 
 			textToSpeech.setQueueMode(TTS.QUEUE_FLUSH);
 		}
@@ -124,33 +129,46 @@ public class FormActivity extends Activity implements OnClickListener,
 	}
 
 	private void setForm2ScreenContent() {
+		ans5 = (RadioGroup) findViewById(R.id.ans5);
+		ans5.setOnFocusChangeListener(this);
+		ans5.requestFocus();
+
 		ans6 = (RadioGroup) findViewById(R.id.ans6);
 		ans6.setOnFocusChangeListener(this);
 		ans6.requestFocus();
 
+		ans7 = (RadioGroup) findViewById(R.id.ans7);
+		ans7.setOnFocusChangeListener(this);
+		ans7.requestFocus();
+
 		ans8 = (RadioGroup) findViewById(R.id.ans8);
 		ans8.setOnFocusChangeListener(this);
 		ans8.requestFocus();
-		
+
+		ans9 = (RadioGroup) findViewById(R.id.ans9);
+		ans9.setOnFocusChangeListener(this);
+		ans9.requestFocus();
+
 		ans10 = (RadioGroup) findViewById(R.id.ans10);
 		ans10.setOnFocusChangeListener(this);
 		ans10.requestFocus();
 
-		ans12 = (RadioGroup) findViewById(R.id.ans12);
-		ans12.setOnFocusChangeListener(this);
-		ans12.requestFocus();
+		ans11 = (MultiAutoCompleteTextView) findViewById(R.id.ans11);
+		// ans11.setOnClickListener(this);
 
-		ans14 = (MultiAutoCompleteTextView) findViewById(R.id.ans14);
-//		ans14.setOnClickListener(this);
-		
+		r5Selected = (RadioButton) findViewById(ans5.getCheckedRadioButtonId());
+		setRadioContent(ans5);
 		r6Selected = (RadioButton) findViewById(ans6.getCheckedRadioButtonId());
 		setRadioContent(ans6);
+		r7Selected = (RadioButton) findViewById(ans7.getCheckedRadioButtonId());
+		setRadioContent(ans7);
 		r8Selected = (RadioButton) findViewById(ans8.getCheckedRadioButtonId());
 		setRadioContent(ans8);
-		r10Selected = (RadioButton) findViewById(ans10.getCheckedRadioButtonId());
+		r9Selected = (RadioButton) findViewById(ans9.getCheckedRadioButtonId());
+		setRadioContent(ans9);
+		r10Selected = (RadioButton) findViewById(ans10
+				.getCheckedRadioButtonId());
 		setRadioContent(ans10);
-		r12Selected = (RadioButton) findViewById(ans12.getCheckedRadioButtonId());
-		setRadioContent(ans12);
 
 	}
 
@@ -162,23 +180,23 @@ public class FormActivity extends Activity implements OnClickListener,
 		ans2 = (RadioGroup) findViewById(R.id.ans2);
 		ans2.setOnFocusChangeListener(this);
 		ans2.requestFocus();
-		
+
+		ans3 = (RadioGroup) findViewById(R.id.ans3);
+		ans3.setOnFocusChangeListener(this);
+		ans3.requestFocus();
+
 		ans4 = (RadioGroup) findViewById(R.id.ans4);
 		ans4.setOnFocusChangeListener(this);
 		ans4.requestFocus();
-
-		ans5 = (RadioGroup) findViewById(R.id.ans5);
-		ans5.setOnFocusChangeListener(this);
-		ans5.requestFocus();
 
 		r1Selected = (RadioButton) findViewById(ans1.getCheckedRadioButtonId());
 		setRadioContent(ans1);
 		r2Selected = (RadioButton) findViewById(ans2.getCheckedRadioButtonId());
 		setRadioContent(ans2);
+		r3Selected = (RadioButton) findViewById(ans3.getCheckedRadioButtonId());
+		setRadioContent(ans3);
 		r4Selected = (RadioButton) findViewById(ans4.getCheckedRadioButtonId());
 		setRadioContent(ans4);
-		r5Selected = (RadioButton) findViewById(ans5.getCheckedRadioButtonId());
-		setRadioContent(ans5);
 	}
 
 	private void setRadioContent(RadioGroup group) {
@@ -186,7 +204,7 @@ public class FormActivity extends Activity implements OnClickListener,
 		while (i < group.getChildCount()) {
 			group.getChildAt(i).setOnFocusChangeListener(this);
 			group.getChildAt(i).setOnClickListener(this);
-			i++;                                       
+			i++;
 		}
 	}
 
@@ -205,18 +223,20 @@ public class FormActivity extends Activity implements OnClickListener,
 		Log l = Log.getLog();
 		l.addAnswer(r1Selected.getText().toString());
 		l.addAnswer(r2Selected.getText().toString());
+		l.addAnswer(r3Selected.getText().toString());
 		l.addAnswer(r4Selected.getText().toString());
-		l.addAnswer(r5Selected.getText().toString());
 	}
 
 	private void manageForm2() {
 		Log l = Log.getLog();
+		l.addAnswer(r5Selected.getText().toString());
 		l.addAnswer(r6Selected.getText().toString());
+		l.addAnswer(r7Selected.getText().toString());
 		l.addAnswer(r8Selected.getText().toString());
+		l.addAnswer(r9Selected.getText().toString());
 		l.addAnswer(r10Selected.getText().toString());
-		l.addAnswer(r12Selected.getText().toString());
-		l.addAnswer(ans14.getText().toString());
-		
+		l.addAnswer(ans11.getText().toString());
+
 		finish();
 	}
 
@@ -228,38 +248,42 @@ public class FormActivity extends Activity implements OnClickListener,
 		if (hasFocus) {
 			textToSpeech.speak(v);
 			textToSpeech.setQueueMode(TTS.QUEUE_ADD);
-			if (groupContainId(ans1, v.getId())){
+			if (groupContainId(ans1, v.getId())) {
 				textToSpeech.speak(getString(R.string.q1_context));
-				ans = 1; 
-			}else if (groupContainId(ans2, v.getId())){
+				ans = 1;
+			} else if (groupContainId(ans2, v.getId())) {
 				textToSpeech.speak(getString(R.string.q2_context));
-				ans = 2; 
-			}else if (ans4 != null && groupContainId(ans4, v.getId())){
-				textToSpeech.speak(getString(R.string.q4_context));
+				ans = 2;
+			} else if (groupContainId(ans3, v.getId())) {
+				textToSpeech.speak(getString(R.string.q3_context));
 				ans = 3;
-			}else if (ans5 != null && groupContainId(ans5, v.getId())){
-				textToSpeech.speak(getString(R.string.q5_context));
+			} else if (ans4 != null && groupContainId(ans4, v.getId())) {
+				textToSpeech.speak(getString(R.string.q4_context));
 				ans = 4;
-			}else if (ans6 != null && groupContainId(ans6, v.getId())){
-				textToSpeech.speak(getString(R.string.q6_context));
+			} else if (ans5 != null && groupContainId(ans5, v.getId())) {
+				textToSpeech.speak(getString(R.string.q5_context));
 				ans = 5;
-			}else if (ans8 != null && groupContainId(ans8, v.getId())){
-				textToSpeech.speak(getString(R.string.q8_context));
+			} else if (ans6 != null && groupContainId(ans6, v.getId())) {
+				textToSpeech.speak(getString(R.string.q6_context));
 				ans = 6;
-			}else if (ans9 != null && groupContainId(ans9, v.getId())){
-				textToSpeech.speak(getString(R.string.instructions_label));
+			}else if (ans7 != null && groupContainId(ans7, v.getId())) {
+				textToSpeech.speak(getString(R.string.q7_context));
 				ans = 7;
-			}else if (ans10 != null && groupContainId(ans10, v.getId())){
-				textToSpeech.speak(getString(R.string.q10_context));
+			} else if (ans8 != null && groupContainId(ans8, v.getId())) {
+				textToSpeech.speak(getString(R.string.q8_context));
 				ans = 8;
-			}else if (ans12 != null && groupContainId(ans12, v.getId())){
-				textToSpeech.speak(getString(R.string.q12_context));
+			} else if (ans9 != null && groupContainId(ans9, v.getId())) {
+				textToSpeech.speak(getString(R.string.instructions_label));
 				ans = 9;
+			} else if (ans10 != null && groupContainId(ans10, v.getId())) {
+				textToSpeech.speak(getString(R.string.q10_context));
+				ans = 10;
 			}
 			textToSpeech.setQueueMode(TTS.QUEUE_FLUSH);
-			
-			AnalyticsManager.getAnalyticsManager().registerAction(MinesweeperAnalytics.FORM_EVENTS, MinesweeperAnalytics.CLICK, 
-					"Ans " + ans, 0);
+
+			AnalyticsManager.getAnalyticsManager().registerAction(
+					MinesweeperAnalytics.FORM_EVENTS,
+					MinesweeperAnalytics.CLICK, "Ans " + ans, 0);
 		}
 	}
 
@@ -272,11 +296,12 @@ public class FormActivity extends Activity implements OnClickListener,
 		}
 		return found;
 	}
-	
-	public void onBackPressed (){
-		Toast toast = Toast.makeText(this, R.string.on_back_pressed, Toast.LENGTH_SHORT);
-		toast.show();
-		textToSpeech.speak(this.getString(R.string.on_back_pressed));
-	}
+
+//	public void onBackPressed() {
+//		Toast toast = Toast.makeText(this, R.string.on_back_pressed,
+//				Toast.LENGTH_SHORT);
+//		toast.show();
+//		textToSpeech.speak(this.getString(R.string.on_back_pressed));
+//	}
 
 }
