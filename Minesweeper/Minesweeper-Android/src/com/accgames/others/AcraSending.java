f4992eced8ee4c72eceed3784b1b53476f1ca8be
Gloria <gepozuelo@gmail.com>
2012-04-28 16:19:29 +0200

    + Añadido Feedback de reporte de errores en todas las apps. + Commiteo los cambios en .classpath (simplemente poner a true las librerías) para que me las detecte xD

---
+++ Minesweeper/Minesweeper-Android/src/com/accgames/others/AcraSending.java
@@ -0,0 +1,15 @@
+package com.accgames.others;
+
+import android.app.Application;
+import org.acra.*;
+import org.acra.annotation.*;
+
+@ReportsCrashes(formKey = "dFNvb2VqZ09DNHhUN2IwZ0lpRkZMY2c6MQ")
+public class AcraSending extends Application {
+	@Override
+	public void onCreate() {
+		// The following line triggers the initialization of ACRA
+		ACRA.init(this);
+		super.onCreate();
+	}
+}
